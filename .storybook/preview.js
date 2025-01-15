4f857cf6a0b5e9a1d11cdd6d3c4abb167ef41506
Taurean Bryant <taurean@hey.com>
2025-01-15 10:12:21 -0800

    some foundational global CSS

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,17 @@
 /** @type { import('@storybook/svelte').Preview } */
+
+import "../src/lib/styles/foundation.css";
+import "../src/lib/styles/storybook.css";
+
 const preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
+    parameters: {
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
     },
-  },
 };
 
 export default preview;
