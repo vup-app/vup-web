7be0a6d4a14b853c8337346d8f374411b56543de
Taurean Bryant <taurean@hey.com>
2025-02-09 18:50:31 -0800

    new breadcrumbs component

---
+++ src/lib/components/Breadcrumbs.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    const { path, onClick } = $props();
+
+    const handleDirChange = (dir: string) => {
+        const clickedPathIndex = path.indexOf(dir);
+        const newPath = path.slice(0, clickedPathIndex + 1);
+        onClick(newPath);
+    };
+</script>
+
+<nav>
+    <ol>
+        {#each path.slice(0, -1) as dir, index}
+            <li>
+                <button onclick={() => handleDirChange(dir)}>{dir}</button>
+            </li>
+        {/each}
+    </ol>
+    <div>{path[path.length - 1]}</div>
+</nav>
