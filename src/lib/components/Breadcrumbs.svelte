9f67a2ef3b7eac6073b16d3ab2bc6eb4164238a5
Taurean Bryant <taurean@hey.com>
2025-02-10 15:04:53 -0800

    updating component logic

--- src/lib/components/Breadcrumbs.svelte
+++ src/lib/components/Breadcrumbs.svelte
@@ -2,19 +2,32 @@
     const { path, onClick } = $props();
 
     const handleDirChange = (dir: string) => {
-        const clickedPathIndex = path.indexOf(dir);
-        const newPath = path.slice(0, clickedPathIndex + 1);
-        onClick(newPath);
+        if (dir == "home") {
+            onClick(dir);
+        } else {
+            const clickedPathIndex = path.indexOf(dir);
+            const newPath = path.slice(0, clickedPathIndex + 1);
+            onClick(newPath);
+        }
     };
 </script>
 
-<nav>
+<nav aria-label="Directory navigation">
     <ol>
-        {#each path.slice(0, -1) as dir, index}
+        {#if path.length > 4}
+            <li>
+                <button onclick={() => handleDirChange("home")}>home</button>
+            </li>
+            <li>
+                <span role="presentation">…</span>
+                <span data-visually-hidden>truncated directory</span>
+            </li>
+        {/if}
+        {#each path.length > 4 ? path.slice(-4, -1) : path.slice(0, -1) as dir, index}
             <li>
                 <button onclick={() => handleDirChange(dir)}>{dir}</button>
             </li>
         {/each}
     </ol>
-    <div>{path[path.length - 1]}</div>
+    <div aria-current={path[path.length - 1]}>{path[path.length - 1]}</div>
 </nav>
---
+++ src/lib/stories/Breadcrumbs.stories.svelte
@@ -0,0 +1,19 @@
+<script module>
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+    import Breadcrumbs from "$lib/components/Breadcrumbs.svelte";
+    import { fn } from "@storybook/test";
+
+    const path = ["file", "path", "here", "current path"];
+
+    // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+    const { Story } = defineMeta({
+        title: "Component/Breadcrumbs",
+        component: Breadcrumbs,
+        tags: ["autodocs"],
+        args: {
+            path: path,
+        },
+    });
+</script>
+
+<Story name="Default" />
