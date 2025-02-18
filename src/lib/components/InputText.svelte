c6ad66880b73d47f7db0a5505ca79e7ae9c74202
Taurean Bryant <later@taurean.work>
2025-02-18 15:56:45 -0800

    drafting new input component

---
+++ src/lib/components/InputText.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { writable } from "svelte/store";
+    const { label, placeholder, value = writable("") } = $props();
+
+    const inputId = crypto.randomUUID();
+
+    const handleInput = (event: Event) => {
+        const target = event.target as HTMLInputElement;
+        value.set(target.value);
+    };
+</script>
+
+<div>
+    <label for={inputId}>{label}</label>
+    <input
+        id={inputId}
+        type="text"
+        placeholder={`${placeholder}…`}
+        oninput={handleInput}
+        bind:value={$value}
+    />
+</div>
+
+<style>
+</style>
---
+++ src/lib/stories/InputText.stories.svelte
@@ -0,0 +1,17 @@
+<script module>
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+    import InputText from "$lib/components/InputText.svelte";
+
+    // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+    const { Story } = defineMeta({
+        title: "Component/Text Input",
+        component: InputText,
+        tags: ["autodocs"],
+        args: {
+            label: "Input Label",
+            placeholder: "Placeholder",
+        },
+    });
+</script>
+
+<Story name="Default" />
