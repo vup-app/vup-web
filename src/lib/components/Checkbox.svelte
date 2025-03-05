693d4d279ac83446bc96aca43c060b6f9606ac2c
Taurean Bryant <later@taurean.work>
2025-03-04 16:10:10 -0800

    updating styles for Checkbox component

--- src/lib/components/Checkbox.svelte
+++ src/lib/components/Checkbox.svelte
@@ -41,25 +41,37 @@
         cursor: pointer;
         display: inline-flex;
         align-items: center;
+        font-weight: 600;
     }
 
     .checkbox + .label::before {
         content: "";
         display: block;
         flex-grow: 1;
-        width: 1em;
-        height: 1em;
+        width: 1.4em;
+        height: 1.4em;
         aspect-ratio: 1 / 1;
-        background-color: red;
-        border: 2px solid blue;
+        border: 3px solid var(--hue-neutral-500);
         margin-inline-end: 10px;
         transition: 75ms;
         padding: 0;
-        outline: 1px solid green;
+        border-radius: var(--radii-default);
+        outline: 4px solid rgba(0, 0, 0, 0);
+    }
+
+    .checkbox:hover + .label::before,
+    .checkbox:focus-within + .label::before {
+        border-color: var(--hue-neutral-700);
+    }
+
+    .checkbox:focus-within + .label::before {
+        outline: 4px solid #fdd3b3;
+        outline-offset: 2px;
     }
 
     .checkbox-wrapper:has(.checkbox:checked) .checkbox + .label::before {
-        outline: 5px solid green;
-        background-color: blue;
+        border-color: var(--hue-emerald-500);
+        background-color: var(--hue-emerald-500);
+        box-shadow: inset 0 0 0 3px white;
     }
 </style>
